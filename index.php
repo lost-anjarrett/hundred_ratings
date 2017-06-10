f45ed6ac4f1cece15f5f8864c54bf3030e46ad3d
Guillaume P <guillaume.poncet.pro@gmail.com>
2017-06-10 16:01:06 +0200

    modif contenu

--- index.php
+++ index.php
@@ -9,36 +9,42 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1 class="text-center border-bottom"><span class="logo">
-            <span class="logo-inner">
-                <span class="logo-2x">%</span>
-                <span class="logo-1x">%</span>
-                <span class="logo-3x">%</span>
-            </span>
-        </span>Hundred Ratings</h1>
+<header>
+    <div class="container clearfix">
+        <a class="left" href="#"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></a>
+        <a class="right" href="https://github.com/lost-anjarrett/hundred_ratings"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+    </div>
+    <h1 class="text-center border-bottom"><span class="logo">
+        <span class="logo-inner">
+            <span class="logo-2x">%</span>
+            <span class="logo-1x">%</span>
+            <span class="logo-3x">%</span>
+        </span>
+    </span><br>Hundred Ratings</h1>
 
-        <p class="text-center border-bottom">An multi-instance percent-rating system based on AJAX</p>
+    <p class="text-center border-bottom">A multi-instance percentage bar-rating system based on AJAX</p>
 
-        <div class="container page-content">
-            <aside id="menu" class="">
-                <nav>
-                    <ul>
-                        <li><a href="#demo">Demo</a></li>
-                        <li><a href="#start">Get started</a>
-                            <ol>
-                                <li><a href="#start">HTML</a></li>
-                                <li><a href="#js">JavaScript</a></li>
-                                <li><a href="#options">Options</a></li>
-                                <li><a href="#more">More</a></li>
-                            </ol>
-                        </li>
-                        <li><a href="#about">About</a></li>
-                    </ul>
-                </nav>
-            </aside>
-            <div id="content"></div>
+</header>
+<main class="container page-content">
+    <aside id="menu" class="">
+        <nav>
+            <ul>
+                <li><a href="#demo">Demo</a></li>
+                <li><a href="#start">Get started</a>
+                    <ol>
+                        <li><a href="#start">HTML</a></li>
+                        <li><a href="#js">JavaScript</a></li>
+                        <li><a href="#options">Options</a></li>
+                        <li><a href="#more">More</a></li>
+                    </ol>
+                </li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </nav>
+    </aside>
+    <div id="content"></div>
 
-        </div>
+</main>
 
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
